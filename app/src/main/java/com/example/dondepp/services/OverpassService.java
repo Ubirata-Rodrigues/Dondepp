8b571f8c4c17fa296f8d146ebc08f70302fd917b
Ubiratã Rodrigues <ubirataaz7@gmail.com>
2025-11-28 11:31:47 -0300

    Correcoes de Bug

--- app/src/main/java/com/example/dondepp/services/OverpassService.java
+++ app/src/main/java/com/example/dondepp/services/OverpassService.java
@@ -3,10 +3,14 @@ package com.example.dondepp.services;
 
 import com.example.dondepp.model.OverpassResponse;
 import retrofit2.Call;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface OverpassService {
-    @GET("interpreter")
+    @GET("interpreter/")
     Call<OverpassResponse> searchPlaces(@Query("data") String query);
+
+
 }
